de7fe802ce19cb8a74fdac0e68201ce239f1b4a3
Ben <33158722+renjygit@users.noreply.github.com>
2023-02-23 14:12:36 +0000

    Update unicode growth only 3.py

--- unicode growth only 3.py
+++ unicode growth only 3.py
@@ -33,22 +33,23 @@ gamma = 0.4 # J m^-2
 kB = 1.38e-23
 k = 0.1 # 1e12 # J mol^-1 m^-1 distribution exponent constant --------------------------------------------
 kr = 1.6e-9
-Damkohler = 1e-3
+Damkohler = 2e3 # 1e-3 # can change up to 2000
 density = 5.8e3 # kg m^-3
 MW = 0.096 # kg mol^-1
 Q = (4*np.pi*density) / (3 * MW * Vtot * MInf)
 
 # Define time bins
-tmax = 5e-6 # 3600
-tdiff = 1e-6 #1e-3
+tmax = 1e1 # 3600
+tdiff = 1e-3 #1e-3
 timeArray = np.linspace(tdiff, tmax, int(tmax/tdiff))
 timeCount = 0
 
 
 # Define radius bins
 rdiff =  3e-11 #0.3 Angstroms
-rmax = 8.1e-9
-rBins = np.linspace(0.5e-9,rmax,int((rmax+rdiff)/rdiff))
+rmax = 3.3e-9 # 8.1e-9
+rmin = 5e-10
+rBins = np.linspace(rmin,rmax,int((rmax-rmin+rdiff)/rdiff))
 
 # Define temp variable & heating rate
 Temp = 503 #Temp = 453
@@ -60,7 +61,7 @@ Ppop = 1e24 # change
 PPopArray = [Ppop]
 
 # Define supersaturation population variable & array
-SS = 5e3
+SS = 5e2
 SSArray = [SS]
 
 # Define nuclei arrays
@@ -71,11 +72,12 @@ print("NNum = " + str(NNum))
 # Create an initial distribution of nanoparticles (to analyse growth stage)
 sum = 0
 for r in rBins:
-    g = NNum * (1 / (1e-10 * np.sqrt((2*np.pi)))) * np.exp((-np.power((r - 1e-9), 2)) / (2*(1e-10)**2)) # distribution with mean radius 1*10^-9 standard deviation of 10%
+    g = NNum * (1 / (1e-10 * np.sqrt((2*np.pi)))) * np.exp(-0.5*(np.power((r - 1e-9)/(1e-10), 2))) # distribution with mean radius 1*10^-9 standard deviation of 10%
     sum+=g*rdiff # check probabilities sum to NNum (approx)
     #print(g)
     NArraysArray[0].append(g) # g going to 0 for all r
 print("sum " + str(sum))
+#print(NArraysArray[0])
 # Define avg array
 #NArraysArray = [[0] * len(rBins)]
 NArrayAvgR = [stats.fmean(NArraysArray[0])]
@@ -195,7 +197,7 @@ for time in timeArray: #start at timestep tdiff not 0
         #print("growthRatepos  " + str(growthRatepos))
             
         # Check Courant condition - just prints warning at present
-        if (growthRatepos * tdiff/rdiff) > 1 or (growthRateneg * tdiff/rdiff) >1:
+        if (np.abs(growthRatepos) * tdiff/rdiff) > 1 or (np.abs(growthRateneg) * tdiff/rdiff) >1:
             print("Courant condition not satisfied.")
             print("Courant value for growthratepos " + str(growthRatepos * tdiff/rdiff))
             print("Courant value for growthrateneg " + str(growthRateneg * tdiff/rdiff))
@@ -211,32 +213,38 @@ for time in timeArray: #start at timestep tdiff not 0
         SSIntegralElement = pow(r,3) * (NArraysArray[1][rCount] - NArraysArray[0][rCount])
         SSSumsArray.append(SSIntegralElement)
 
-    plt.plot(rBins, NHalfPosList)
-    plt.title("NHalfPos at t = " + str(time))
-    plt.show()
-    plt.plot(rBins, NHalfNegList)
-    plt.title("NHalfNeg at t = " + str(time))
-    plt.show()
+    #plt.plot(rBins, NHalfPosList)
+    #plt.title("NHalfPos at t = " + str(time))
+    #plt.show()
+    #plt.plot(rBins, NHalfNegList)
+    #plt.title("NHalfNeg at t = " + str(time))
+    #plt.show()
     
     # Add average radius of N to array
     NArrayAvgR.append([stats.fmean(NArraysArray[1])])
+
     #print("Avg N " + str(NArrayAvgR[timeCount]))
     # Delete N array for previous timestep (to conserve memory since it is no longer needed)
+    #print(NArraysArray)
     del NArraysArray[0]
+    #print(NArraysArray)
 
     """
     # Calculate molar precursor concentration
     PConc = Ppop / (Vtot * NA)
     """
     
+    print("sum of ss array = " + str(np.sum(SSSumsArray)))
     # Calculate SS value for this timestep & add to array
-    SS += - (Q * rdiff*np.sum(SSSumsArray)) # Sum all integral element values to approximate the integral, no precursor part for growth (just have an initial pop of monomer)
+    SS += - (Q * np.sum(SSSumsArray)) # Sum all integral element values to approximate the integral, no precursor part for growth (just have an initial pop of monomer)
     SSArray.append(SS)
     print("SS " + str(SS))
     #print("   ")
     #print(NArraysArray[0])
     
+    print("navgarray = " + str(NArrayAvgR))
 
+    
 
 timeArrayFull = np.linspace(0, tmax, int((tmax/tdiff) + 1)) # Create new time array to include t = 0
 
