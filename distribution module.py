c69e701ba8873672aa9071f5b37c0a6e21e22a5b
Ben <33158722+renjygit@users.noreply.github.com>
2023-03-07 11:39:58 +0000

    Update distribution module.py

--- distribution module.py
+++ distribution module.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb  6 11:21:42 2023
-@author: BWCoo
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,7 +5,7 @@ import matplotlib.pyplot as plt
 A = 4e3
 PMEa = 4.5e4 # J mol^-1
 R = 8.314
-VmMolar = 1.65e-5 # m^3 mol^-1, molar volume of monomer
+VmMolar = 3.2996e-5 #1.65e-5 # m^3 mol^-1, molar volume of monomer
 rmMolar = (3/4*np.pi) * pow(VmMolar, 1/3) # effective radius of monomer (from the molar volume)
 rm = 1.87e-10
 Vm = (4/3)*np.pi*pow(rm,3)
@@ -26,6 +20,7 @@ kr = 1#1.6e-9 # -------------------------------------------
 density = 5.8e3 # kg m^-3
 MW = 0.096 # kg mol^-1
 Q = (4*np.pi*density) / 3 * MW * Vtot * MInf
+k = 1e12 # J mol^-1
 
 # Define temp variable & heating rate
 Temp = 453
@@ -33,14 +28,15 @@ Tf = 567
 HR = 0.025 # K s^-1 (1.5 K min^-1)
 
 # Define supersaturation population variable
-SS = 5e3
+SS = 5e2
 
 
-sigma = np.sqrt(kB*Temp/2)
+sigma = np.sqrt((kB*Temp)/2) # (kB*Temp/2)
+print("sigma = " + str(sigma))
 
 
 rCrit = (2*gamma*VmMolar) / (R*Temp*np.log(SS))
-print("rCrit " + str(rCrit))
+print("rCrit = " + str(rCrit))
 r = 0
 
 """
@@ -58,18 +54,25 @@ rdiff =  3e-11 #0.3 Angstroms
 rmax = 8.1e-9
 rBins = np.linspace(0,rmax,int((rmax+rdiff)/rdiff)*1)
 
-gArray = []
+gArray1 = []
+gArray2 = []
 # Calculate the distribution across the r values in rBins
 for r in rBins:
-    giteration = (1 / (1e-10 * np.sqrt((2*np.pi)))) * np.exp((-np.power((r - 1e-9), 2)) / (2*(1e-10)**2)) # distribution with mean radius 1*10^-9 standard deviation of 10%
+    giteration1 = (1 / (1e-10 * np.sqrt((2*np.pi)))) * np.exp((-np.power((r - 1e-9), 2)) / (2*(1e-10)**2)) # distribution with mean radius 1*10^-9 standard deviation of 10%
 
-    #giteration = (1/(sigma*np.sqrt((2*np.pi)))) * np.exp((-k*np.power((r-rCrit), 2)) / (2*np.power(sigma, 2))) 
-    gArray.append(giteration)
-
+    giteration2 = (1 / (sigma * np.sqrt((2*np.pi)))) * np.exp((-np.power((r - rCrit), 2)) / (2*np.power(sigma, 2)))
+    
+    gArray1.append(giteration1)
+    
+    gArray2.append(giteration2)
+    
+    
+    
 #g = np.random.normal(1e-9, 1e-10, size = 270)
 #g = g.tolist()
 #print(g)
 # Plot the distribution
 #plt.scatter(rBins,g)
-plt.plot(rBins,gArray)
+plt.plot(rBins, gArray1)
+plt.plot(rBins, gArray2)
 plt.show()
